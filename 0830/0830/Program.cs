3daf2a2123709cdc438a5296141b1ad66a96bdec
gustaf-kingq <gvmw@live.com>
2016-08-30 12:39:07 +0200

    0830 - Added Code Added unecessarily long filler text that asked two question, one about the users first and one about their last name that then returns it in the form of "Hej" + n1 + n2 + "!".

--- 0830/0830/Program.cs
+++ 0830/0830/Program.cs
@@ -10,6 +10,14 @@ namespace _0830
     {
         static void Main(string[] args)
         {
+            System.Console.Write("Since there weren't any said prohibition on the length of the greeting segment of this code I am not only writing it in English instead of Swedish but also doing it longer than necessary. Oh yeah, what is your first name?");
+            Console.WriteLine("");
+            string n1 = Console.ReadLine();
+            System.Console.Write("Well isn't that a name. " + n1 + ". What's your last name then? This other question is ending this whole English-longer-than-necessary-bit and we'll go back to Swedish after I'v recieved your answer.");
+            Console.WriteLine("");
+            string n2 = Console.ReadLine();
+            Console.Write("Hej " + n1 + n2 + "!");
+            Console.ReadLine();
         }
     }
 }
