71f1108c9cfe0265ea5bc9901b361fd49e9fe7c7
gustaf-kingq <gvmw@live.com>
2016-08-30 13:18:42 +0200

    0830 - Added formality Removed the long lines for easier reading and replaced the earlier questions and lines with more formal and friendly substitutes. The user should be at as much ease as possible while using someones code.

--- 0830/0830/Program.cs
+++ 0830/0830/Program.cs
@@ -10,13 +10,14 @@ namespace _0830
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Since there weren't any said prohibition on the length of the greeting segment of this code I am not only writing it in English instead of Swedish but also doing it longer than necessary. Oh yeah, what is your first name?");
+            System.Console.Write("Good day, sir/ma'am. What is your first name?");
             Console.WriteLine("");
             string n1 = Console.ReadLine();
-            System.Console.Write("Well isn't that a name. " + n1 + ". What's your last name then? This other question is ending this whole English-longer-than-necessary-bit and we'll go back to Swedish after I'v recieved your answer.");
+            System.Console.Write("Noted. Your first name is " + n1 + ".");
+            System.Console.Write("Next and final question: What is your last name?");
             Console.WriteLine("");
             string n2 = Console.ReadLine();
-            Console.Write("Hej " + n2 + "!");
+            Console.Write("Welcome aboard, Mr. or Miss " + n2 + "!");
             Console.ReadLine();
         }
     }
