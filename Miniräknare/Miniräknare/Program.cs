e9dbe91d7b73daa763c69a0133a8659b12d7c563
gustaf-kingq <gvmw@live.com>
2016-09-01 09:36:00 +0200

    Commit..

---
+++ Miniräknare/Miniräknare/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Miniräknare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.Beep();
+
+            string s;
+            s = Console.ReadLine();
+
+            int i;
+            i = Int32.Parse(s);
+            Console.WriteLine(s);
+
+        }
+    }
+}
