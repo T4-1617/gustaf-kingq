60db082c68687b88b0d554d794392eeb9889cb0e
gustaf-kingq <gvmw@live.com>
2016-09-01 14:08:48 +0200

    0901 - 2.1-2.7 The code now returns: - 2.1, the amount of chars with space in between, - 2.2, the amount of chars with no space in between, - 2.3, the amount of chars in the first name (title cased), - 2.4, the amount of chars in the last name (title cased), - 2.5, title cased first name after last name and - 2.6, vertically capitalized first and last name.

--- 0901/0901/Program.cs
+++ 0901/0901/Program.cs
@@ -10,51 +10,70 @@ namespace _0901
     {
         static void Main(string[] args)
         {
+            //1
             Console.WriteLine("::AXCEL.INC NAME-MESSER-UPPER:: BOOTING UP...");
             Console.WriteLine("What is your first name?");
             string name1 = Console.ReadLine();
 
             Console.WriteLine("Good. What is your last name?");
             string name2 = Console.ReadLine();
+            string name1_normal = name1.ToUpper()[0] + name1.Substring(1);
+            string name2_normal = name2.ToUpper()[0] + name2.Substring(1);
+            string fullname = name1_normal + " " + name2_normal;
 
-            string name1_upper = name1.ToUpper();
 
-            string name2_upper = name2.ToUpper();
+            //2.1 - 
+            Console.WriteLine("Number of chars with space in " + fullname + " is: " + fullname.Length);
 
 
-            string fullname = name1 + " " + name2;
-
-            Console.WriteLine("Number of chars + space in " + fullname + " is: " + fullname.Length);
-
+            //2.2 - 
             int chars_no_space = fullname.Length - 1;
+            Console.WriteLine("Number of chars without space in " + fullname.Trim() + " is: " + chars_no_space);
 
 
-            Console.WriteLine("Number of chars without space in " + fullname + " is: " + chars_no_space);
-
-            Console.WriteLine("Your first name is " + name1.ToUpper([0]) + " and has: " + name1.Length + " chars.");
-
-            Console.WriteLine("Your first name is " + name2.ToUpper([0]) + " and has: " + name2.Length + " chars.");
+            //2.3 - 
+            Console.WriteLine("Your first name is " + name1_normal + " and has: " + name1.Length + " chars."); 
 
 
-            Console.WriteLine("In the opposite order, in a more business fashion, your name looks like this: " + name2 + " " + name1);
+            //2.4 - 
+            Console.WriteLine("Your first name is " + name2_normal + " and has: " + name2.Length + " chars.");
 
 
+            //2.5 - 
+            Console.WriteLine("In the opposite order, in a more business fashion, your name looks like this: " + name2_normal + " " + name1_normal);
+
+
+            //2.6 - 
             Console.WriteLine("You name vertically printed looks like: ");
-            for (int i = 0; i < name1_upper.Length; i++)
+            for (int i = 0; i < name1.Length; i++)
             {
-                
+
                 Console.Write("----->");
-                Console.WriteLine(name1_upper[i]);
+                Console.WriteLine(name1.ToUpper()[i]);
             }
 
-            for (int i = 0; i < name2_upper.Length; i++)
+            for (int i = 0; i < name2.Length; i++)
             {
 
                 Console.Write("------->");
-                Console.WriteLine(name2_upper[i]);
+                Console.WriteLine(name2.ToUpper()[i]);
             }
 
 
+            //2.7 - 
         }
+            public static void Reverse(string name1)
+        {
+            char[] cArray = name1.ToCharArray();
+            string name1_reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                name1_reverse += cArray[i];
+            }
+
+            Console.WriteLine("Your name with lower letters looks like: " + name1_reverse);
+        }
+
+
     }
 }
