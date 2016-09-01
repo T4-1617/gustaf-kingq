817c21704aaface53f7056b38a22a71e94d05b65
gustaf-kingq <gvmw@live.com>
2016-09-01 12:13:25 +0200

    0901 - Initial Commit First commit with code accepting entry of a first and last name whilst combining the two to a seperate string for further use.

---
+++ 0901/0901/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0901
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::AXCEL.INC NAME-MESSER-UPPER:: BOOTING UP...");
+            Console.WriteLine("What is your first name?");
+            string name1 = Console.ReadLine();
+
+            Console.WriteLine("Good. What is your last name?");
+            string name2 = Console.ReadLine();
+
+            string fullname = name1 + " " + name2;
+
+
+        }
+    }
+}
