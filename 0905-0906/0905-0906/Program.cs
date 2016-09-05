d44ef515ac1a38343013acf6cfb4f3c000726c03
gustaf-kingq <gvmw@live.com>
2016-09-05 15:09:31 +0200

    0905-0906 - Initial commit

---
+++ 0905-0906/0905-0906/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0905_0906
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
