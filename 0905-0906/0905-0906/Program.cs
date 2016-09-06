80549cb57a4ead610d15d71f7f9404b17131bda9
gustaf-kingq <gvmw@live.com>
2016-09-06 14:36:15 +0200

    0905-0906 - Basic variables Added the three founding variables: colors, groups and the amount of cards. But also the counter that will help with the future loop.

--- 0905-0906/0905-0906/Program.cs
+++ 0905-0906/0905-0906/Program.cs
@@ -10,6 +10,20 @@ namespace _0905_0906
     {
         static void Main(string[] args)
         {
+            //We are going to create a deck of cards, shuffle them, give
+            //input for additional cards and generally play around with
+            //how the output message will behave
+
+            //First above all else should be the three basic variables:
+            //the colors of the cards, the groups and the amount of cards
+            int colors = 4;
+            int groups = 13;
+            int amountOfCards = 52;
+
+            //And a counter to make progress possible
+            int counter = 0;
+
+
         }
     }
 }
