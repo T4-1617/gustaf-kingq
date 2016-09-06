8fc710199dcf9663df991e0db4f263773cc22abb
gustaf-kingq <gvmw@live.com>
2016-09-06 15:49:15 +0200

    0905-0906 - Enhanced and Initialized StrArr Enhanced with more documentation to go with the removal of the hovering static void Main method since not every block were supposed to be static. The basic ints are now constant since the code won't work with them in any other condition. The string array is now initialized and variables for two color choosers are now made. The counter is gonna be moved down for when it's needed.

--- 0905-0906/0905-0906/Program.cs
+++ 0905-0906/0905-0906/Program.cs
@@ -8,22 +8,41 @@ namespace _0905_0906
 {
     class Program
     {
-        static void Main(string[] args)
-        {
             //We are going to create a deck of cards, shuffle them, give
             //input for additional cards and generally play around with
             //how the output message will behave
 
+            //
+
             //First above all else should be the three basic variables:
             //the colors of the cards, the groups and the amount of cards
-            int colors = 4;
-            int groups = 13;
-            int amountOfCards = 52;
+            const int colors = 4;
+            const int groups = 13;
+            const int amountOfCards = 52;
 
-            //And a counter to make progress possible
-            int counter = 0;
+            //Up next is the main two arrays, one string containing colors
+            //and groups and a boolean that records if the card has already
+            //been played or not
+            static string[,] StringDeck = new String[colors, groups];
+            static bool[,] BoolDeck = new bool[colors, groups];
+
+            //Initializing the string array is essential
+            static string[,] cardDeckInit(string[,] StringDeck)
+        {
+            //Now we need a proper variable for the choosing of the colors
+            //For now, the four are represented by 1-4 in the order
+            //Hearts, Diamonds, Spades and Clubs
+            int colorChooser = 1;
+
+            //To ease the task, let's make an associate variable for
+            //colorChooser to hold any values
+            //It should be created empty
+            string colorChooser2 = string.Empty;
 
 
         }
+
+
     }
-}
+    }
+
