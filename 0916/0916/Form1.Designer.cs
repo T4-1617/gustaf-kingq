eadbaf239561392822a6728e8667359a4a956b87
gustaf-kingq <gvmw@live.com>
2016-09-16 09:52:19 +0200

    0916 - Initial Commit/Form structure Added the four sections: - Overall buttons controlling showcase of available, addition oc new and return of rented cars, - Panel1, showing the available cars in a listbox and the ability to book with a button, - Panel2, allowing the addition of class items for a new car including make, model and color and a button to add the items to make a new car for rent and - Panel3, a boxlst of rented cars and a return button making that car available again and moves from this boxlist back to panel1.

---
+++ 0916/0916/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace _0916
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonShowCar = new System.Windows.Forms.Button();
+            this.buttonAddCar = new System.Windows.Forms.Button();
+            this.buttonReturnCar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelAdd = new System.Windows.Forms.Panel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.panelReturn = new System.Windows.Forms.Panel();
+            this.listBoxBooked = new System.Windows.Forms.ListBox();
+            this.buttonReturn = new System.Windows.Forms.Button();
+            this.panelShow = new System.Windows.Forms.Panel();
+            this.listBoxShow = new System.Windows.Forms.ListBox();
+            this.buttonBook = new System.Windows.Forms.Button();
+            this.textBoxMake = new System.Windows.Forms.TextBox();
+            this.textBoxModel = new System.Windows.Forms.TextBox();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            this.textBoxReg = new System.Windows.Forms.TextBox();
+            this.textBoxSeats = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panelAdd.SuspendLayout();
+            this.panelReturn.SuspendLayout();
+            this.panelShow.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonShowCar
+            // 
+            this.buttonShowCar.Location = new System.Drawing.Point(25, 94);
+            this.buttonShowCar.Name = "buttonShowCar";
+            this.buttonShowCar.Size = new System.Drawing.Size(93, 41);
+            this.buttonShowCar.TabIndex = 0;
+            this.buttonShowCar.Text = "Show Cars";
+            this.buttonShowCar.UseVisualStyleBackColor = true;
+            this.buttonShowCar.Click += new System.EventHandler(this.buttonShowCar_Click);
+            // 
+            // buttonAddCar
+            // 
+            this.buttonAddCar.Location = new System.Drawing.Point(124, 94);
+            this.buttonAddCar.Name = "buttonAddCar";
+            this.buttonAddCar.Size = new System.Drawing.Size(93, 41);
+            this.buttonAddCar.TabIndex = 1;
+            this.buttonAddCar.Text = "Add a Car";
+            this.buttonAddCar.UseVisualStyleBackColor = true;
+            this.buttonAddCar.Click += new System.EventHandler(this.buttonAddCar_Click);
+            // 
+            // buttonReturnCar
+            // 
+            this.buttonReturnCar.Location = new System.Drawing.Point(223, 94);
+            this.buttonReturnCar.Name = "buttonReturnCar";
+            this.buttonReturnCar.Size = new System.Drawing.Size(93, 41);
+            this.buttonReturnCar.TabIndex = 2;
+            this.buttonReturnCar.Text = "Return a Car";
+            this.buttonReturnCar.UseVisualStyleBackColor = true;
+            this.buttonReturnCar.Click += new System.EventHandler(this.buttonReturnCar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "We have 3 cars available.";
+            // 
+            // panelAdd
+            // 
+            this.panelAdd.Controls.Add(this.label7);
+            this.panelAdd.Controls.Add(this.label6);
+            this.panelAdd.Controls.Add(this.label5);
+            this.panelAdd.Controls.Add(this.label4);
+            this.panelAdd.Controls.Add(this.label3);
+            this.panelAdd.Controls.Add(this.label2);
+            this.panelAdd.Controls.Add(this.textBoxPrice);
+            this.panelAdd.Controls.Add(this.textBoxSeats);
+            this.panelAdd.Controls.Add(this.textBoxReg);
+            this.panelAdd.Controls.Add(this.textBoxColor);
+            this.panelAdd.Controls.Add(this.textBoxModel);
+            this.panelAdd.Controls.Add(this.textBoxMake);
+            this.panelAdd.Controls.Add(this.buttonAdd);
+            this.panelAdd.Location = new System.Drawing.Point(25, 160);
+            this.panelAdd.Name = "panelAdd";
+            this.panelAdd.Size = new System.Drawing.Size(405, 256);
+            this.panelAdd.TabIndex = 5;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(272, 196);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(93, 41);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // panelReturn
+            // 
+            this.panelReturn.Controls.Add(this.listBoxBooked);
+            this.panelReturn.Controls.Add(this.buttonReturn);
+            this.panelReturn.Location = new System.Drawing.Point(25, 159);
+            this.panelReturn.Name = "panelReturn";
+            this.panelReturn.Size = new System.Drawing.Size(405, 256);
+            this.panelReturn.TabIndex = 8;
+            // 
+            // listBoxBooked
+            // 
+            this.listBoxBooked.FormattingEnabled = true;
+            this.listBoxBooked.ItemHeight = 16;
+            this.listBoxBooked.Location = new System.Drawing.Point(10, 10);
+            this.listBoxBooked.Name = "listBoxBooked";
+            this.listBoxBooked.Size = new System.Drawing.Size(250, 228);
+            this.listBoxBooked.TabIndex = 7;
+            this.listBoxBooked.SelectedIndexChanged += new System.EventHandler(this.listBoxBooked_SelectedIndexChanged);
+            // 
+            // buttonReturn
+            // 
+            this.buttonReturn.Location = new System.Drawing.Point(272, 196);
+            this.buttonReturn.Name = "buttonReturn";
+            this.buttonReturn.Size = new System.Drawing.Size(93, 41);
+            this.buttonReturn.TabIndex = 6;
+            this.buttonReturn.Text = "Return";
+            this.buttonReturn.UseVisualStyleBackColor = true;
+            this.buttonReturn.Click += new System.EventHandler(this.buttonReturn_Click);
+            // 
+            // panelShow
+            // 
+            this.panelShow.Controls.Add(this.listBoxShow);
+            this.panelShow.Controls.Add(this.buttonBook);
+            this.panelShow.Location = new System.Drawing.Point(25, 160);
+            this.panelShow.Name = "panelShow";
+            this.panelShow.Size = new System.Drawing.Size(405, 256);
+            this.panelShow.TabIndex = 8;
+            // 
+            // listBoxShow
+            // 
+            this.listBoxShow.FormattingEnabled = true;
+            this.listBoxShow.ItemHeight = 16;
+            this.listBoxShow.Location = new System.Drawing.Point(10, 10);
+            this.listBoxShow.Name = "listBoxShow";
+            this.listBoxShow.Size = new System.Drawing.Size(250, 228);
+            this.listBoxShow.TabIndex = 7;
+            this.listBoxShow.SelectedIndexChanged += new System.EventHandler(this.listBoxShow_SelectedIndexChanged);
+            // 
+            // buttonBook
+            // 
+            this.buttonBook.Location = new System.Drawing.Point(273, 196);
+            this.buttonBook.Name = "buttonBook";
+            this.buttonBook.Size = new System.Drawing.Size(93, 41);
+            this.buttonBook.TabIndex = 6;
+            this.buttonBook.Text = "Book";
+            this.buttonBook.UseVisualStyleBackColor = true;
+            this.buttonBook.Click += new System.EventHandler(this.buttonBook_Click);
+            // 
+            // textBoxMake
+            // 
+            this.textBoxMake.Location = new System.Drawing.Point(99, 24);
+            this.textBoxMake.Name = "textBoxMake";
+            this.textBoxMake.Size = new System.Drawing.Size(161, 22);
+            this.textBoxMake.TabIndex = 7;
+            this.textBoxMake.TextChanged += new System.EventHandler(this.textBoxMake_TextChanged);
+            // 
+            // textBoxModel
+            // 
+            this.textBoxModel.Location = new System.Drawing.Point(99, 52);
+            this.textBoxModel.Name = "textBoxModel";
+            this.textBoxModel.Size = new System.Drawing.Size(161, 22);
+            this.textBoxModel.TabIndex = 8;
+            this.textBoxModel.TextChanged += new System.EventHandler(this.textBoxModel_TextChanged);
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.Location = new System.Drawing.Point(99, 80);
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(161, 22);
+            this.textBoxColor.TabIndex = 9;
+            this.textBoxColor.TextChanged += new System.EventHandler(this.textBoxColor_TextChanged);
+            // 
+            // textBoxReg
+            // 
+            this.textBoxReg.Location = new System.Drawing.Point(99, 108);
+            this.textBoxReg.Name = "textBoxReg";
+            this.textBoxReg.Size = new System.Drawing.Size(161, 22);
+            this.textBoxReg.TabIndex = 10;
+            this.textBoxReg.TextChanged += new System.EventHandler(this.textBoxReg_TextChanged);
+            // 
+            // textBoxSeats
+            // 
+            this.textBoxSeats.Location = new System.Drawing.Point(99, 136);
+            this.textBoxSeats.Name = "textBoxSeats";
+            this.textBoxSeats.Size = new System.Drawing.Size(161, 22);
+            this.textBoxSeats.TabIndex = 11;
+            this.textBoxSeats.TextChanged += new System.EventHandler(this.textBoxSeats_TextChanged);
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(99, 164);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(161, 22);
+            this.textBoxPrice.TabIndex = 12;
+            this.textBoxPrice.TextChanged += new System.EventHandler(this.textBoxPrice_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Make:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Model:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Color:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Reg. Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Seats:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 164);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 16);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Price:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 445);
+            this.Controls.Add(this.panelShow);
+            this.Controls.Add(this.panelAdd);
+            this.Controls.Add(this.panelReturn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReturnCar);
+            this.Controls.Add(this.buttonAddCar);
+            this.Controls.Add(this.buttonShowCar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelAdd.ResumeLayout(false);
+            this.panelAdd.PerformLayout();
+            this.panelReturn.ResumeLayout(false);
+            this.panelShow.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonShowCar;
+        private System.Windows.Forms.Button buttonAddCar;
+        private System.Windows.Forms.Button buttonReturnCar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelAdd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxSeats;
+        private System.Windows.Forms.TextBox textBoxReg;
+        private System.Windows.Forms.TextBox textBoxColor;
+        private System.Windows.Forms.TextBox textBoxModel;
+        private System.Windows.Forms.TextBox textBoxMake;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Panel panelReturn;
+        private System.Windows.Forms.ListBox listBoxBooked;
+        private System.Windows.Forms.Button buttonReturn;
+        private System.Windows.Forms.Panel panelShow;
+        private System.Windows.Forms.ListBox listBoxShow;
+        private System.Windows.Forms.Button buttonBook;
+    }
+}
+
---
+++ 0916/0916/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0916
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShowCar_Click(object sender, EventArgs e){}
+        private void buttonAddCar_Click(object sender, EventArgs e){}
+        private void buttonReturnCar_Click(object sender, EventArgs e){}
+
+        private void listBoxShow_SelectedIndexChanged(object sender, EventArgs e){}
+        private void buttonBook_Click(object sender, EventArgs e){}
+
+        private void textBoxMake_TextChanged(object sender, EventArgs e){}
+        private void textBoxModel_TextChanged(object sender, EventArgs e){}
+        private void textBoxColor_TextChanged(object sender, EventArgs e){}
+        private void textBoxReg_TextChanged(object sender, EventArgs e){}
+        private void textBoxSeats_TextChanged(object sender, EventArgs e){}
+        private void textBoxPrice_TextChanged(object sender, EventArgs e){}
+        private void buttonAdd_Click(object sender, EventArgs e){}
+
+        private void listBoxBooked_SelectedIndexChanged(object sender, EventArgs e){}
+        private void buttonReturn_Click(object sender, EventArgs e){}
+    }
+}
